76d6b2cd35c181ce67a82d2439915066c56f5b0c
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-15 20:24:55 -0400

    Update EventFormPage.js

--- client/src/components/pages/EventFormPage/EventFormPage.js
+++ client/src/components/pages/EventFormPage/EventFormPage.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import CommunityJumbo from "../../components/CommunityJumbotron/CommunityJumbo";
+import NewsJumbo from "../../components/NewsJumbo/NewsJumbo";
 
 
 function EventFormPage() {
     return (
-        <main class="mt-10 w-screen mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 shadow-2xl">
-            <div class="sm:text-center lg:text-center">
-            <h1 class="text-4xl mt-15 tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span class="block text-red-200 xl:inline"> Welcome to the </span>
-              <span class="block text-white xl:inline"> Community Page!!! </span>
-            </h1>
+        <main>
+            <div>
+                < Navbar />
+                < div class="container jumbotronContainer">
+                < CommunityJumbo />
+                < NewsJumbo />
+            </div>
             <h3 class="text-4xl mt-15 tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span class="block text-blue-200 xl:inline"> Where you can get the most recent updates </span>
+              <span class="block text-blue-200 xl:inline"> Where you can help make real change... </span>
               <span class="block text-blue xl:inline"> and help make sure everyone has the right to vote...   </span>
             </h3>
 
