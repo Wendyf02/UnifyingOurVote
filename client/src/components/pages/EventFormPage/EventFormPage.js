02f6afd0f37e251b952519882631212a4938d4a7
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-15 20:25:08 -0400

    Update EventFormPage.js

--- client/src/components/pages/EventFormPage/EventFormPage.js
+++ client/src/components/pages/EventFormPage/EventFormPage.js
@@ -1,45 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import CommunityJumbo from "../../components/CommunityJumbotron/CommunityJumbo";
 import NewsJumbo from "../../components/NewsJumbo/NewsJumbo";
+// import Capitol from "../../assets/images/Capitol"
+
 
 
 function EventFormPage() {
     return (
         <main>
             <div>
-                < Navbar />
+                <Navbar/>
                 < div class="container jumbotronContainer">
-                < CommunityJumbo />
-                < NewsJumbo />
-            </div>
-            <h3 class="text-4xl mt-15 tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span class="block text-blue-200 xl:inline"> Where you can help make real change... </span>
-              <span class="block text-blue xl:inline"> and help make sure everyone has the right to vote...   </span>
-            </h3>
-
-            <br></br>
-            <br></br>
-
-            <div class="mt-10 w-screen mx-auto shadow-inner">
-                <div class="mt-15 text-center text-white font-bold text-4xl bg-gradient-to-r from-green-600 to-green-700">
-                    <br></br>
-                    <div class="max-w-auto text-center justify-center mx-8xl">
-                        <h2>
-                            News Article
-                        </h2>
-                        <p>
-                            A free forum to share and discuss favorite reads. 
-                        </p>
-                    </div>
-                    <br></br>
+                    < CommunityJumbo />
+                    < NewsJumbo />
                 </div>
-            </div>
+                <h3 class="text-4xl mt-15 tracking-tight    font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <span class="block text-blue-200 xl:inline"> Where you can help make real change... </span>
+                    <span class="block text-blue xl:inline"> and help make sure everyone has the right to vote...   </span>
+                </h3>
+
+                <br></br>
+                <br></br>
+
+                
+
+
 
             </div>
-            <Link to="/login"> Placeholder link to login </Link>
-            <Link to="/register"> Placeholder link to register </Link>
         </main>
     )
 }
