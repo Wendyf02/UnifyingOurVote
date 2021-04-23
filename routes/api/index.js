036ed1d0e7efe4bd8ba0b138e962e6796457082f
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-04-22 21:17:54 -0400

    Deleted unnecessary file.

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const bookRoutes = require("./books");
-
-// Book routes
-router.use("/books", bookRoutes); //calls the first api file above.Books.js
-
-module.exports = router;
