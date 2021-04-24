50b5d0641f4c08b73a882c6b437d8e16ad114b76
turtles <me@ramon.nyc>
2021-04-24 12:23:55 -0700

    Update App.js
    redirected import to functional js from tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import CommunityPage from "./pages/CommunityPage";
 import { BrowserRouter as Router,Switch, Route} from "react-router-dom";
 import Login from "./components/Login/login.component"
-import Signup from "./components/Signup";
+import Signup from "./components/Signup/register.component";
 import Jumbotron from "./components/Jumbotron"
 import Home from "./components/Home"
 import "bootstrap/dist/css/bootstrap.min.css";
