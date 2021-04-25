0baba064ab3336d5d8dc797edaa7db6e519035dc
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-04-25 14:19:29 -0400

    Minor changes and added eventNav and styling with the ActualEvent component which is also called in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,43 @@
 import React from "react";
 import Footer from "./components/Footer";
 import CommunityPage from "./pages/CommunityPage";
-import { BrowserRouter as Router,Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Jumbotron from "./components/Jumbotron"
-import Home from "./components/Home"
-
-import EventFormPage from "./components/EventFormPage/index";
-
-import "./App.css" 
-
+import Jumbotron from "./components/Jumbotron";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import EventFormPage from "./components/EventFormPage";
+import "./App.css";
+import ActualEvent from "./components/ActualEvent";
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          
           <Route exact path={["/"]}>
             <Jumbotron />
             <Home />
           </Route>
 
-          <Route exact path="/login">
-            <Login/>
-          </Route>
-          
+          <Route exact path="/login" component={Login} />
+
           <Route exact path="/signup">
-            <Signup/>
+            <Signup />
           </Route>
 
-          <Route exact path={["/YourCommunity", "/events"]}
-            component={CommunityPage} />
-          
-          <Route exact path="/CreateEvent"
-           component={EventFormPage} />
+          <Route
+            exact
+            path={["/YourCommunity", "/events"]}
+            component={CommunityPage}
+          />
 
-           {/* <Route exact path="/events/:id" component={}/> */}
+          <Route exact path="/CreateEvent" component={EventFormPage} />
+          {/* Should connect to specific id for each event coming from database. */}
+          <Route exact path="/events/:id" component={ActualEvent} />
         </Switch>
-        <Footer/>
-
+        <Footer />
       </div>
     </Router>
   );
---
+++ client/src/components/ActualEvent/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import EventNavbar from "../EventNavbar";
+
+function ActualEvent() {
+  return (
+    <div>
+        <EventNavbar/>
+This is the event page with the nav bar.
+    </div>
+  )
+}
+export default ActualEvent;
+
