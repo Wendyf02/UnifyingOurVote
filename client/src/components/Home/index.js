9ba65cd2fa9fb80d9d7dbeedb18d3077ca01be9f
Wendy Fgigueroa <camomila444@gmail>
2021-04-26 19:21:09 -0400

    upadted link bttns and appbaout

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -24,7 +24,6 @@ function Home() {
         <a href="/login"><Button className="login"  variant="outline-danger">Log In</Button>{' '} </a>
       </div>
 
-    
       
       <img className="home2 mx-auto d-block" src={home2} /> 
       
@@ -38,35 +37,39 @@ function Home() {
       <img className="about2 mx-auto d-block" id="about2" src={about1} /> 
      
       <Row className="justify-content-md-center"> 
-        {/* <Col xs="12" sm="12" lg="7">
-           <img className="about1" src={about1}/> 
-        </Col> */}
-        <Col xs sm="12" lg="5" className="pl-2">
-          <p>
+       
+        <Col xs sm="12" lg="5" className="pl-4">
+          <p className="aboutapp">
+          <br/>
+              <br/>
              The purpose of our App is to get peolpe in the Georgia Community, a place to gather, organize events and educate with   
              the mission of staying active in the fight of the current law passed by the Georgia Legislative.         
-
+              <br/>
+              <br/>
              This unlawful law is aimed to disenfranchise and make it harder for African American and minorities to vote.    
              As a community we must Unify and Unite to this unjust law.   
-
+             <br/>
+              <br/>
              We must not turn the blind eye and stay silent and stand on the side lines.   
-
              We must unite and continue the mission our ancestor started. Lets educate, let's make our voices resound,
-             let our liberties and rights stand tall and strong.Let's make them proud and honor them with vindication.
-
-             AS Martin Luther KIng Jr. Once said "Life's most persistent and urgent question is "What are you doing for others?"
+             let our liberties and rights stand tall and strong.Let's make them proud and honor them with vindication.   
+             <br/>
+              <br/>
+             As Martin Luther King Jr. Once said "Life's most persistent and urgent question is "What are you doing for others?"
              This selfless approach to this date has created positive inspiring change to all future generations.
-
+             <br/>
+              <br/>
              Several movements have sprung due to changing times and circumstances. The Black Lives Matter and the influential Stacy Abrams
-             are our present day examples which aims towards the onggoing evolving struggles that minorities face in present times.
-
+             are our present day examples which aims towards the ongoing evolving struggles that minorities face in present times.
+             <br/>
+              <br/>
              Keep connected get involve.              
           </p>
         </Col>
       </Row>
 
       <div className="d-flex justify-content-center"> 
-        {/* <a target="blank" href="https://thekingcenter.org"> <Button className="link1"  variant="outline-danger">TKC</Button>{' '}</a>  */}
+        
         <a target="blank" href="https://staceyabrams.com"> <Button className="link2"  variant="outline-danger">Stacy Abrams</Button>{' '}</a>
         <a target="blank" href="https://blacklivesmatter.com"> <Button className="link3"  variant="outline-danger">blacklivesmatter</Button>{' '}</a>
       </div>
